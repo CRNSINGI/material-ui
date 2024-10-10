ad82942f6b55872d1cce0da12779cf97e725e1b1
Cesar <cesarnsingi@hotmail.com>
2024-10-10 10:17:36 +0400

    add js and fix src

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input_field");
+const inputIcon = document.querySelector(".input_icon");
+
+inputIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+})
